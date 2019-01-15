5c7a7e150619b6e458ca75cbd3a0616896715dfd
Radu <radupetrisel@gmail.com>
2019-01-15 21:18:40 +0200

    init

---
+++ Assets/CubeSpawner.cs
@@ -0,0 +1,18 @@
+﻿using Leap;
+using Leap.Unity;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+
+    public CapsuleHand LeftHand;
+    public CapsuleHand RightHand;
+    public GameObject Cube;
+
+    public void Spawn()
+    {
+        Instantiate(Cube, RightHand.GetLeapHand().GetThumb().TipPosition.ToVector3(), Quaternion.identity);
+    }
+
+
+}
